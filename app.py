8cab71f9765895bea0d47dddb0e0f32d9d542e9a
tcaty <ltcatyl@gmail.com>
2022-03-22 19:11:33 +0300

    endpoint /test-schedule/<string:group> with prepackaged schedule added;

--- app.py
+++ app.py
@@ -5,19 +5,20 @@ from flask_cors import CORS, cross_origin
 
 from scraper import get_schedule
 from utils import quote_text
+from test_schedule import test_schedule
 
 
-app = Flask(__name__)
-CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
+application = Flask(__name__)
+CORS(application)
+application.config['CORS_HEADERS'] = 'Content-Type'
 
 
-@app.route('/')
+@application.route('/')
 def say_hello():
     return 'Hello! It is simple API for tsu schedule :)'
 
 
-@app.route('/schedule/<string:group>', methods=['GET'])
+@application.route('/schedule/<string:group>', methods=['GET'])
 @cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
 def send_schedule(group):
     response_data = {
@@ -27,5 +28,10 @@ def send_schedule(group):
     return json.dumps(response_data)
 
 
+@application.route('/test-schedule/<string:group>', methods=['GET'])
+def send_test_schedule(group):
+    return json.dumps(test_schedule)
+
+
 if __name__ == '__main__':
-    app.run(debug=False)
+    application.run(host='0.0.0.0', debug=False)
